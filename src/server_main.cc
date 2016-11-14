8c64f044eab0dbd59105706569ce8adfdc82b486
JamesC159 <combs.james1993@gmail.com>
2016-11-14 16:52:07 -0600

    Server update

--- src/server_main.cc
+++ src/server_main.cc
@@ -13,13 +13,13 @@ const int MAX_BUF = 8192;
 const int MAX_CONN = 25;
 
 bool
-createSocket ( int * sockDesc ) {
+createSocket ( int & sockDesc ) {
    
-   *sockDesc = socket ( AF_INET,
+   sockDesc = socket ( AF_INET,
                        SOCK_STREAM,
                        0 );
    
-   if ( *sockDesc < 0 ) {
+   if ( sockDesc < 0 ) {
       return false;
    }
    
@@ -27,12 +27,12 @@ createSocket ( int * sockDesc ) {
 }
 
 bool
-bindSocket ( int * sockDesc,
-                 struct sockaddr_in * sockAddr ) {
+bindSocket ( int & sockDesc,
+                 struct sockaddr_in & sockAddr ) {
    
-   if ( bind( *sockDesc,
-             (struct sockaddr *)sockAddr,
-             (socklen_t)sizeof( *sockAddr )) < 0 ) {
+   if ( bind( sockDesc,
+             (struct sockaddr *) & sockAddr,
+             (socklen_t)sizeof( sockAddr )) < 0 ) {
       
       return false;
    }
@@ -41,9 +41,9 @@ bindSocket ( int * sockDesc,
 }
 
 bool
-listenSocket ( int * sockDesc ) {
+listenSocket ( int & sockDesc ) {
    
-   if ( listen ( *sockDesc, MAX_CONN ) < 0 ) {
+   if ( listen ( sockDesc, MAX_CONN ) < 0 ) {
       return false;
    }
    
@@ -51,15 +51,16 @@ listenSocket ( int * sockDesc ) {
 }
 
 bool
-acceptSocket ( int * servDesc,
-                   int * cliDesc,
-                   struct sockaddr_in * cliAddr ) {
+acceptSocket ( int & servDesc,
+               int & cliDesc,
+               struct sockaddr_in & cliAddr,
+               socklen_t & cliLen ) {
    
-   *cliDesc = accept( *servDesc,
-                    (struct sockaddr *)cliAddr,
-                    (socklen_t *)sizeof( *cliAddr ) );
+   cliDesc = accept( servDesc,
+                    (struct sockaddr *) & cliAddr,
+                     & cliLen );
    
-   if ( *cliDesc < 0 ) {
+   if ( cliDesc < 0 ) {
       return false;
    }
    
@@ -70,11 +71,16 @@ acceptSocket ( int * servDesc,
 int
 main ( int argc, char ** argv ) {
    
-   struct sockaddr_in serverAddr, clientAddr;
-   int serverDesc = -1, clientDesc = 0;
-   int bytes = 0, portNo = -1;
-   char buffer [ MAX_BUF ];
-   char * endptr;
+   struct sockaddr_in serverAddr;   // Server socket address structure
+   struct sockaddr_in clientAddr;   // Client socket address structure
+   int serverDesc = -1;             // Server socket descriptor
+   int clientDesc = 0;              // Client socket descriptor
+   int bytes = 0;                   // Bytes read or written to sockets
+   int portNo = -1;                 // Port number server is binded to
+   socklen_t clientLen = 0;         // Length of the client sockaddr_in structure
+   char buffer [ MAX_BUF ];         // Buffer for reading from the client socket
+   char * endptr;                   // Ptr for error handling in strtol()
+   char * msg;                      // Message to write to the client socket
    
    // Make sure the port number was provided in the command line arguments.
    if ( argc < 2 ) {
@@ -90,37 +96,48 @@ main ( int argc, char ** argv ) {
    }
    
    // Create socket for server. Could handle failure here. For now, just terminating.
-   if ( ! createSocket( &serverDesc ) ) {
+   if ( ! createSocket( serverDesc ) ) {
       perror ( "ERROR creating server socket " );
       return 1;
    }
    
    // Zero out the server/client socket address structure.
    bzero ( ( char * ) & serverAddr, sizeof( serverAddr ) );
-   bzero ( ( char * ) & clientAddr, sizeof( clientAddr ) )
-   ;
+   bzero ( ( char * ) & clientAddr, sizeof( clientAddr ) );
+   
    // Set the socket family, port number, and IP address.
    serverAddr.sin_family = AF_INET;
    serverAddr.sin_port = htons( portNo );
    serverAddr.sin_addr.s_addr = INADDR_ANY;
    
    // Bind the listener socket to an address.
-   if ( ! bindSocket ( &serverDesc, &serverAddr ) ) {
+   if ( ! bindSocket ( serverDesc, serverAddr ) ) {
       perror ( "ERROR binding socket to an address " );
       return 1;
    }
    
    // Listen
-   if ( ! listenSocket( &serverDesc ) ) {
+   if ( ! listenSocket( serverDesc ) ) {
       perror( "ERROR listening for socket connections " );
       return 1;
    }
    
+   // Store length of the client
+   clientLen = sizeof( clientAddr );
+   
    // Start client listen-accept phase.
    while ( true ) {
       
+      if ( ! acceptSocket(serverDesc,
+                          clientDesc,
+                          clientAddr,
+                          clientLen ) ) {
+         perror( "ERROR failed to accept socket connection " );
+         return 1;
+      }
+      
       //Accept
-      if ( ! acceptSocket( &serverDesc, &clientDesc, &clientAddr ) ) {
+      if ( clientDesc < 0 ) {
          perror( "ERROR accepting new socket connection " );
          return 1;
       }
@@ -137,8 +154,14 @@ main ( int argc, char ** argv ) {
       printf( "Received message: %s\n", buffer );
       
       // Write message to the client.
-      char * msg = "Received your message\n";
+      msg = (char *)calloc( MAX_BUF, sizeof( msg ) );
+      msg = "Received your message";
       bytes = write (clientDesc, msg, sizeof( msg ) - 1 );
+      if ( bytes < 0 ) {
+         perror( "ERROR: failed writing to socket " );
+         return 1;
+      }
+      free ( msg );
    }
    
    // Close client and server socket descriptors.
