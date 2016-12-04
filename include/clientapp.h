19b8e14947b83002225b28457ff5e2ad9689ba63
Joel P Seida <jps140430@dc04.utdallas.edu>
2016-12-04 00:32:38 -0600

    Added missing code and includes from move

--- include/clientapp.h
+++ include/clientapp.h
@@ -1,6 +1,18 @@
 #ifndef CLIENTAPP_H
 #define CLIENTAPP_H
 
+#include <thread>
+#include <mutex>
+#include <sstream>
+#include <iostream>
+#include <string>
+
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "getch.h"
 
 extern char ownName [32];
 extern char otherName [32];
---
+++ include/getch.h
@@ -0,0 +1,12 @@
+#ifndef GETCH_H
+#define GETCH_H
+
+
+
+char getch(void);
+char getche(void);
+
+
+
+
+#endif
