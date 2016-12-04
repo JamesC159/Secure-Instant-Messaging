20d4ee3bb0782dea3d506f1bca38a60f7f5b0bca
Joel P Seida <jps140430@dc04.utdallas.edu>
2016-12-04 00:21:29 -0600

    Started moving application code in from other codebase

---
+++ include/clientapp.h
@@ -0,0 +1,16 @@
+#ifndef CLIENTAPP_H
+#define CLIENTAPP_H
+
+
+extern char ownName [32];
+extern char otherName [32];
+
+
+
+void startTalking(int sock);
+void sockListener(int sock);
+
+
+
+
+#endif
---
+++ src/clientapp.cc
@@ -0,0 +1,87 @@
+char ownName [32];
+char otherName [32];
+bool done;
+int charsRead;
+std::stringstream ss;
+std::mutex screenLock;
+
+void startTalking(int sock)
+{
+   done = false;
+   charsRead = 0;
+   ss.str("");
+   std::thread lthread(sockListener, sock);
+   while(true)
+   {
+      char c = getch();
+      screenLock.lock();
+      if (done)
+      {
+         charsRead = 0;
+	 ss.str("");
+	 printf("\n");
+	 break;
+      }
+      if (c == '\n')
+      {
+	 write(sock, ss.str().c_str(), charsRead);
+	 printf("\r%s\r", std::string(charsRead, ' ').c_str());
+	 printf("%s: %s\n", ownName, ss.str().c_str());
+	 if (ss.str() == "EXIT")
+	 {
+	    done = true;
+            charsRead = 0;
+	    ss.str("");
+	    break;
+	 }
+         charsRead = 0;
+	 ss.str("");
+      }
+      else
+      {
+         printf("%c", c);
+         charsRead++;
+         ss << c;
+      }
+      screenLock.unlock();
+   }
+   screenLock.unlock();
+   lthread.join();
+}
+
+void sockListener(int sock)
+{
+   char buff [1024];
+   memset(buff, '\0', sizeof(buff));
+   while (true)
+   {
+      int cRead = read(sock, buff, sizeof(buff)-1);
+      if (cRead > 0)
+      {
+         buff[cRead] = '\0';
+	 screenLock.lock();
+	 printf("\r%s\r", std::string(charsRead, ' ').c_str());
+	 printf("%s: %s\n", otherName, buff);
+	 if (std::string(buff) == "EXIT")
+	 {
+	    if (!done)
+	    {
+	       write(sock, buff, strlen(buff));
+	       printf("Session has ended, press a key to continue...");
+	       done = true;
+	    }
+	    screenLock.unlock();
+	    break;
+	 }
+	 printf("%s", ss.str().c_str());
+	 flush(std::cout);
+	 screenLock.unlock();
+	 memset(buff, '\0', sizeof(buff));
+      }
+      else if (cRead < 0)
+      {
+         fprintf(stderr, "ERROR: read returned with < 0\n");
+	 return;
+      }
+   }
+}
